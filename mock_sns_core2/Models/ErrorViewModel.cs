c21c927cdf4a8ccb9f1c788640fa8ba0edb7a95d
Mizuno66 <kouta126@hotmail.co.jp>
2020-04-23 16:10:49 +0900

    プロジェクト作成

---
+++ mock_sns_core2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace mock_sns_core2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
