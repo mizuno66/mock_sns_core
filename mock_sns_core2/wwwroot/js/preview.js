36f1bffe0b3f903bf64ccb5bb0eb6088a28bd988
Mizuno66 <kouta126@hotmail.co.jp>
2020-05-18 21:34:57 +0900

    複数画像のプレビュー機能 無限スクロール実装

---
+++ mock_sns_core2/wwwroot/js/preview.js
@@ -0,0 +1,19 @@
+﻿document.getElementById('image-select').addEventListener('change', function (e) {
+    var list = e.target.files;
+
+    var preview = document.getElementById("preview");
+    preview.textContent = null;
+
+    for (let i = 0; i < list.length; i++) {
+        var file = e.target.files[i];
+
+        var blobUrl = window.URL.createObjectURL(file);
+
+        var img = document.createElement("img");
+        img.src = blobUrl;
+        img.width = 100;
+        img.height = 100;
+
+        preview.appendChild(img);
+    }
+});
